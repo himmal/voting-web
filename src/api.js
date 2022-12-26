b6bddd6384946f3bf482e3849e5fa64dbb2e6f67
Him Lam <hohimlam123@gmail.com>
2022-12-27 06:20:06 +0800

    feat: auto deployment
    feat: github workflow
    
    fix: deployment script
    
    fix: enviroment type
    
    fix: add uses to job
    
    fix: steps invalid syntax
    
    fix: update npm to yarn
    
    fix: deployment name
    
    fix: add pkg install
    
    fix: yml syntax
    
    fix: deployment script
    
    fix: remove CI
    
    fix: add user config
    
    fix: deploy name
    
    fix: deploy script
    
    fix: script syntax
    
    fix: force push origin
    
    fix: remove origin setting
    
    fix: push build only
    
    fix: add origin
    
    fix: add build folder only
    
    fixup! fix: push build only
    
    fix: add force commit flag
    
    fix: add origin
    
    fix: add github token
    
    fix: add token prefix
    
    fix: add default server url
    
    fix: homepage update
    
    fix: reset server url error msg

--- src/api.js
+++ src/api.js
@@ -1,18 +1,17 @@
-import { connect } from 'socket.io-client'
-
+import { connect } from "socket.io-client";
 
 function getEnv(name) {
-    let value = process.env[name]
-    if (!value) {
-        let error = Error('missing environment variable: ' + name)
-        document.body.innerText = error.message
-        throw error
-    }
-    return value
+  let value = process.env[name];
+  if (!value) {
+    let error = Error("missing environment variable: " + name);
+    document.body.innerText = error.message;
+    throw error;
+  }
+  return value;
 }
 
-export let API_ORIGIN = getEnv('REACT_APP_API_SERVER')
+export let API_ORIGIN = getEnv("REACT_APP_API_SERVER");
 
 export function connectSocketIO() {
-    return connect(API_ORIGIN)
+  return connect(API_ORIGIN);
 }
